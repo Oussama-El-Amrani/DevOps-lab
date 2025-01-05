6b0a8231b1297e74a0d05d7acc73165022dc5422
Oussama-El-Amrani <oussama.elamrani4@usmba.ac.ma>
2025-01-05 20:32:53 +0100

    feat(mongoService): implement utility functions for MongoDB operations
    - Implemented a generic findAll function for retrieving documents.
    - Implemented createDocument function to add new documents to the collection.
    - Added export for utility functions to allow reuse in other parts of the application.

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -1,14 +1,42 @@
 // Question: Pourquoi créer des services séparés ?
 // Réponse: 
 
-const { ObjectId } = require('mongodb');
+const { ObjectId, Collection } = require("mongodb");
 
 // Fonctions utilitaires pour MongoDB
+/**
+ *
+ * @param {Collection<Document>} collection
+ * @param {ObjectId} id
+ * @returns
+ */
 async function findOneById(collection, id) {
-  // TODO: Implémenter une fonction générique de recherche par ID
+  return await collection.findOne({ _id: new ObjectId(id) });
+}
+
+/**
+ *
+ * @param {Collection<Document>} collection
+ * @param {ObjectId} id
+ * @returns
+ */
+async function findAll(collection) {
+  return await collection.find().toArray();
+}
+
+/**
+ *
+ *
+ * @param {Collection<Document>} collection
+ * @param {*} document
+ */
+async function createDocument(collection, document) {
+  return await collection.insertOne(document);
 }
 
 // Export des services
 module.exports = {
-  // TODO: Exporter les fonctions utilitaires
+  findOneById,
+  findAll,
+  createDocument,
 };
