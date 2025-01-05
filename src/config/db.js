7beac3ac1a2dd540d82497d45192c384fe2931a9
Oussama-El-Amrani <oussama.elamrani4@usmba.ac.ma>
2025-01-05 20:23:25 +0100

    feat(db): implement MongoDB connection
    - Added MongoDB connection logic
    - Implemented getDb function to safely access the database instance.

--- src/config/db.js
+++ src/config/db.js
@@ -3,15 +3,51 @@
 // Question : Comment gérer proprement la fermeture des connexions ?
 // Réponse : 
 
-const { MongoClient } = require('mongodb');
-const redis = require('redis');
-const config = require('./env');
-
-let mongoClient, redisClient, db;
+const { MongoClient, Db } = require("mongodb");
+MongoClient;
+const redis = require("redis");
+const config = require("./env");
+/**
+ * @type {MongoClient} mongoClient
+ */
+let mongoClient;
+let redisClient;
+/**
+ * @type {Db} db
+ */
+let db;
 
 async function connectMongo() {
-  // TODO: Implémenter la connexion MongoDB
   // Gérer les erreurs et les retries
+  try {
+    mongoClient = new MongoClient(config.mongodb.uri);
+
+    await mongoClient.connect();
+
+    db = mongoClient.db(config.mongodb.dbName);
+    console.log("✅ MongoDB Connected");
+
+    return db;
+  } catch (err) {
+    console.error("❌ Error:", err);
+  }
+}
+
+async function closeMongo() {
+  if (mongoClient) {
+    await mongoClient.close();
+    console.log("MongoDB connection Closed");
+  }
+}
+
+/**
+ *
+ * @returns  @type {Db} db
+ */
+function getDb() {
+  if (!db) throw new Error("Database not initialized. Call connectMongo first");
+
+  return db;
 }
 
 async function connectRedis() {
